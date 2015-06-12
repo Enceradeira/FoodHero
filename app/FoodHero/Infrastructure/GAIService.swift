30af20008af50cd8f5966dfe9a2c2031c8a6ca2d
Jorg Jenni <jorg.jenni@jennius.co.uk>
2015-06-12 12:40:26 +0100

    GAI.dryRun used when Simulator

--- app/FoodHero/Infrastructure/GAIService.swift
+++ app/FoodHero/Infrastructure/GAIService.swift
@@ -9,10 +9,9 @@ public class GAIService: NSObject {
     private static var _tracker: GAITracker! = nil
 
     public class func configure() -> Bool {
-        if UIDevice.currentDevice().model.rangeOfString("Simulator") != nil {
-            return false
-        }
+        let isInSimulator =  UIDevice.currentDevice().model.rangeOfString("Simulator") != nil
 
+        GAI.sharedInstance().dryRun = isInSimulator
         GAI.sharedInstance().trackUncaughtExceptions = true
         GAI.sharedInstance().logger.logLevel = GAILogLevel.Verbose
         GAI.sharedInstance().dispatchInterval = 60
@@ -25,42 +24,23 @@ public class GAIService: NSObject {
         _tracker.set("&uid", value: UserId.id())
         return true
     }
-
-    private class var gaiInactive: Bool{
-        get{
-            return _tracker == nil
-        }
-    }
-
     public class func logScreenViewed(screenName: String) {
-        if (gaiInactive) {
-            return;
-        }
         _tracker.set(kGAIScreenName, value: screenName)
         _tracker.send(GAIDictionaryBuilder.createScreenView().build() as [NSObject:AnyObject])
     }
 
     public class func logEventWithCategory(category: String, action: String, label: String, value: Float) {
-        if (gaiInactive) {
-            return;
-        }
         let event = GAIDictionaryBuilder.createEventWithCategory(category, action: action, label: label, value: value).build();
         _tracker.send(event as [NSObject:AnyObject])
 
     }
 
     public class func logTimingWithCategory(category: String, name: String, label: String, interval: NSTimeInterval) {
-        if (gaiInactive) {
-            return;
-        }
         let timing = GAIDictionaryBuilder.createTimingWithCategory(category, interval: interval * 1000, name: name, label: label).build();
         _tracker.send(timing as [NSObject:AnyObject])
     }
 
     public class func dispatch() {
-        if (gaiInactive) {
-            return;
-        }
         GAI.sharedInstance().dispatch()
     }
 }
