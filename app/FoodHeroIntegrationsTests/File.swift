c2f76dbef985716d5ef3199aa6b8e0f3eb298b19
Jorg Jenni <jorg.jenni@jennius.co.uk>
2015-06-30 07:22:01 +0100

    target FoodHeorIntegrationsTests configured for swift

---
+++ app/FoodHeroIntegrationsTests/File.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  FoodHero
+//
+//  Created by Jorg on 30/06/2015.
+//  Copyright (c) 2015 JENNIUS LTD. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class File: XCTestCase{
+    func test_A(){
+        
+    }
+}
