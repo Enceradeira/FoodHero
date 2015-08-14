d03612312cb9dc0b8fe61c35da5f80e5d393b7ea
Jorg Jenni <jorg.jenni@jennius.co.uk>
2015-08-14 08:48:18 +0100

    appium driver setup is not encapsulated in own method

--- features/support/appium.rb
+++ features/support/appium.rb
@@ -2,31 +2,40 @@ require 'appium_lib'
 require_relative '../../lib/app_paths'
 require_relative 'bubble_cache'
 
-# setup driver
-caps = {
-    caps: {
-        platformName: 'iOS',
-        platformVersion: AppPaths.version,
-        deviceName: 'iPhone 5s', # one from 'instruments -s devices'
-        #locationServicesEnabled: false,
-        #locationServicesAuthorized: false,
-        #autoAcceptAlerts: false,
-        #noReset: false,
-        # app: AppPaths.app_path,
-        app: AppPaths.build_path, # WORKAROUND: FoodHero.app (with swiftcode) is sometimes invalid when copied from above AppPaths.app_path
-        processArguments: '-environment=Integration'
-    },
-    appium_lib: {
-        sauce_username: nil, # don't run on Sauce
-        sauce_access_key: nil
-    }
-}
-driver = Appium::Driver.new(caps)
-
 # Promote appium methods into the cucumber world
 class AppiumWorld
+  class << self
+    def setup_driver
+      # setup driver
+      caps = {
+          caps: {
+              platformName: 'iOS',
+              platformVersion: AppPaths.version,
+              deviceName: 'iPhone 5s', # one from 'instruments -s devices'
+              #locationServicesEnabled: false,
+              #locationServicesAuthorized: false,
+              #autoAcceptAlerts: false,
+              #noReset: false,
+              # app: AppPaths.app_path,
+              app: AppPaths.build_path, # WORKAROUND: FoodHero.app (with swiftcode) is sometimes invalid when copied from above AppPaths.app_path
+              processArguments: '-environment=Integration'
+          },
+          appium_lib: {
+              sauce_username: nil, # don't run on Sauce
+              sauce_access_key: nil
+          }
+      }
+      @driver = Appium::Driver.new(caps)
+      @driver.start_driver
+      @driver.no_wait
+      @driver
+    end
+
+    def quit_driver
+      @driver.driver_quit
+    end
+  end
 end
-Appium.promote_appium_methods AppiumWorld
 
 # BubbleCache
 module BubbleCacheWorld
@@ -39,8 +48,6 @@ module BubbleCacheWorld
   end
 end
 
-BubbleCacheWorld.cache = BubbleCache.new(driver)
-
 # extend cucumber steps
 World(BubbleCacheWorld) do
   AppiumWorld.new
@@ -48,10 +55,13 @@ end
 
 # let cucumber control driver
 Before('@app') do
-  driver.start_driver
-  driver.no_wait
+  driver = AppiumWorld.setup_driver
+  Appium.promote_appium_methods AppiumWorld
+
+  BubbleCacheWorld.cache = BubbleCache.new(driver)
   BubbleCacheWorld.cache.reset
 end
+
 After('@app') do
-  driver.driver_quit
+  AppiumWorld.quit_driver
 end
